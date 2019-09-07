0a51f652e6024f43c998f2eefd26a3a81f107544
shubhamsng7 <shubhamsng7@gmail.com>
2019-09-07 12:22:27 +0530

    getch() added

---
+++ mark1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+
+    int a = 10;
+    cout<<"Learning Git and Github"<<endl;
+    cout<<"Basics of Git and Github";
+    cout<<a;
+
+    getch();
+    return 0;
+    //This is our first version..
+}
